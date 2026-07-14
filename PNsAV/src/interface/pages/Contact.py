458c9f8784b08ccdebd524ca0c2d8ddaf6071153
Rares Cosma <cosma.rares.gabriel@gmail.com>
2026-07-14 03:57:55 +0300

    fixed navbar on every page

--- PNsAV/src/interface/pages/Contact.py
+++ PNsAV/src/interface/pages/Contact.py
@@ -14,27 +14,44 @@ with open("style.css", "r") as style_file:
 
 st.html(f"<style>{styles}</style>")
 
-nav_col1, nav_col2, nav_col3, nav_col4 = st.columns([7, 1, 1, 1])
-with open("pnsav_logo.PNG", "rb") as image_file:
-    encoded_logo = base64.b64encode(image_file.read()).decode()
+nav_container = st.container(key="navbar")
+with nav_container:
+    nav_col1, nav_col2, nav_col3, nav_col4 = st.columns([7, 1, 1, 1])
 
-with nav_col1:
-    st.html(f"""
-    <div class="nav-bar">
-        <div class="nav-logo">
-            <a href="http://localhost:8501" id="logo"><img src="data:image/png;base64,{encoded_logo}" alt="Logo" style="height: 80px; width: auto; object-fit: contain;"></a>
+    st.html("""
+    <style>
+    .st-key-navbar div[data-testid="stHorizontalBlock"] {
+        align-items: center !important;
+    }
+    .st-key-navbar div[data-testid="stColumn"] {
+        display: flex !important;
+        align-items: center !important;
+        justify-content: center !important;
+    }
+    </style>
+    """)
+
+    with open("pnsav_logo.PNG", "rb") as image_file:
+        encoded_logo = base64.b64encode(image_file.read()).decode()
+
+    with nav_col1:
+        st.html(f"""
+        <div class="nav-bar">
+            <div class="nav-logo">
+                <a href="http://localhost:8501" id="logo"><img src="data:image/png;base64,{encoded_logo}" alt="Logo" style="height: 80px; width: auto; object-fit: contain;"></a>
+            </div>
         </div>
-    </div>
-""")
-with nav_col2:
-    if st.button("Resources", use_container_width=True):
-        st.switch_page("pages/Resources.py")
-with nav_col3:
-    if st.button("About", use_container_width=True):
-        st.switch_page("pages/About.py")
-with nav_col4:
-    if st.button("Contact", use_container_width=True):
-        st.switch_page("pages/Contact.py")
+    """)
+
+    with nav_col2:
+        if st.button("Resources", use_container_width=True):
+            st.switch_page("pages/Resources.py")
+    with nav_col3:
+        if st.button("About", use_container_width=True):
+            st.switch_page("pages/About.py")
+    with nav_col4:
+        if st.button("Contact", use_container_width=True):
+            st.switch_page("pages/Contact.py")
 
 st.html("<div style='margin-bottom: 0px;'></div>")
 
